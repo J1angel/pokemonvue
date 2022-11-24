f3d4b7c4f04806e5c2cb13df036359790e6fa12c
J1angel <60635557+J1angel@users.noreply.github.com>
2022-11-24 18:33:40 +0800

    initial setup: router, vueitify, vuex, router

---
+++ src/components/NavigationView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-app-bar
+        app
+        absolute
+        color="white"
+        elevation="1"
+        height="100"
+    >
+
+      <v-icon size="100px" color="teal lighten-1">mdi-car-estate</v-icon>
+
+      <v-toolbar-title style="font-size: x-large" v-if="!$vuetify.breakpoint.mobile">Car Maker</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-btn color="teal lighten-1" v-if="authenticate" dark @click="logout">LOG OUT</v-btn>
+    </v-app-bar>
+  </div>
+
+</template>
+<script>
+import {get} from "vuex-pathify";
+import store from "@/store";
+export default {
+  computed:{
+    authenticate:get('authuser/isAuthenticated')
+  },
+  watch:{
+    'authenticate':{
+      handler(newVal) {
+        console.log(newVal)
+      },
+      deep: true
+    },
+  },
+  methods:{
+    logout(){
+      store.dispatch('authuser/logoutuser')
+    }
+  }
+}
+
+</script>
